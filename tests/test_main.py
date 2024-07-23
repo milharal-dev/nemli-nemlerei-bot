b8842167bfbdf13df9723bb53d2534f67e7f1a58
Manoel Vilela <manoel_vilela@engineer.com>
2024-07-23 11:42:34 -0300

    Add nemli template and basic tests structure

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+import pytest
+
+
+@pytest.fixture
+def env_fixture(monkeypatch):
+    """Simulate proper env vars"""
+    monkeypatch.setenv("NEMLI_DISCORD_TOKEN", "xxx")
+    monkeypatch.setenv("NEMLI_DISCORD_PREFIX", "yyy",)
+    yield
+
+
+# Example test
+def test_stupid_import_main(env_fixture):
+    from nemli import main
+
+    assert main
