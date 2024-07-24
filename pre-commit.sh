b00f5c56947de50e5d2a90e76ba710fd950ba073
Mateus Reis <mateuspsvreis@gmail.com>
2024-07-24 02:16:09 -0300

    refactor: almost there..

--- pre-commit.sh
+++ pre-commit.sh
@@ -4,27 +4,27 @@
 set -e  # Exit immediately if a command exits with a non-zero status.
 
 echo "🔄 - Attempting to update dependencies..."
-if pdm install 2>&1 | tee /dev/tty; then
-  echo "✅ - All dependencies are up to date!"
+if stdbuf -oL -eL pdm install | tee /dev/tty; then
+  echo "✅ - All dependencies are up to date!" | stdbuf -oL -eL tee /dev/tty
 else
-  echo "❌ - Some dependencies are out of date!"
+  echo "❌ - Some dependencies are out of date!" | stdbuf -oL -eL tee /dev/tty
   exit 1
 fi
 
 echo "🔄 - Attempting to format code..."
-if pdm format 2>&1 | tee /dev/tty; then
-  echo "✅ - Files formatted successfully!"
+if stdbuf -oL -eL pdm format | tee /dev/tty; then
+  echo "✅ - Files formatted successfully!" | stdbuf -oL -eL tee /dev/tty
 else
-  echo "❌ - Failed to format code!"
+  echo "❌ - Failed to format code!" | stdbuf -oL -eL tee /dev/tty
   exit 1
 fi
 
 echo "🔄 - Attempting to run test suite..."
-if pdm tests 2>&1 | tee /dev/tty; then
-  echo "✅ - Test suite passed!"
+if stdbuf -oL -eL pdm tests | tee /dev/tty; then
+  echo "✅ - Test suite passed!" | stdbuf -oL -eL tee /dev/tty
 else
-  echo "❌ - Test suite failed!"
+  echo "❌ - Test suite failed!" | stdbuf -oL -eL tee /dev/tty
   exit 1
 fi
 
-echo "🚀 - Pre-commit hook completed successfully!"
+echo "🚀 - Pre-commit hook completed successfully!" | stdbuf -oL -eL tee /dev/tty
