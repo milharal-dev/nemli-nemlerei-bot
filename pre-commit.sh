3e54838e3ab2442d96e2d35353c697c826c086fc
Mateus Reis <mateuspsvreis@gmail.com>
2024-07-24 02:21:00 -0300

    refactor: actual final touches for pre-commit.sh

--- pre-commit.sh
+++ pre-commit.sh
@@ -5,24 +5,22 @@ set -e  # Exit immediately if a command exits with a non-zero status.
 
 echo "🔄 - Attempting to update dependencies..."
 if stdbuf -oL -eL pdm install | tee /dev/tty; then
-  echo -e "✅ - All dependencies are up to date!\n🔄 - Attempting to format code now..." | stdbuf -oL -eL tee /dev/tty
+  echo -e "✅ - All dependencies are up to date!\n\n🔄 - Attempting to format code now..." | stdbuf -oL -eL tee /dev/tty
 else
   echo "❌ - Some dependencies are out of date!" | stdbuf -oL -eL tee /dev/tty
   exit 1
 fi
 
 if stdbuf -oL -eL pdm format | tee /dev/tty; then
-  echo -e "✅ - Files formatted successfully!\n🔄 - Attempting to run test suite now..." | stdbuf -oL -eL tee /dev/tty
+  echo -e "✅ - Files formatted successfully!\n\n🔄 - Attempting to run test suite now..." | stdbuf -oL -eL tee /dev/tty
 else
   echo "❌ - Failed to format code!" | stdbuf -oL -eL tee /dev/tty
   exit 1
 fi
 
 if stdbuf -oL -eL pdm tests | tee /dev/tty; then
-  echo "✅ - Test suite passed!" | stdbuf -oL -eL tee /dev/tty
+  echo -e "✅ - Test suite passed!\n\n🚀 - I am now committing this!" | stdbuf -oL -eL tee /dev/tty
 else
   echo "❌ - Test suite failed!" | stdbuf -oL -eL tee /dev/tty
   exit 1
 fi
-
-echo "🚀 - I am now committing this!" | stdbuf -oL -eL tee /dev/tty
