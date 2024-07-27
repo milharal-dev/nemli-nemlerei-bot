0de6bd91ef6094773066c5e54734e5d126c1a3ed
⟠ Rodolfo De Nadai <rdenadai@gmail.com>
2024-07-27 18:04:04 -0300

    feat: Added more prompt types (#46)

--- nemli/schemas/prompt_style.py
+++ nemli/schemas/prompt_style.py
@@ -1,25 +1,38 @@
 from types import MappingProxyType
 
+PARTICIPANTS = "e descrevendo a participação de cada usuário."
 EXTRA = "Agrupe os assuntos por atos e cenas, relacionando os assuntos com os usuários que participaram da discussão."
 
 prompt_types = MappingProxyType(
     {
-        "padrão": (
-            "Resuma a seguinte conversa por assuntos, agrupando por similaridade e descrevendo a participação de"
-            " cada usuário."
-        ),
+        "padrão": f"Resuma a seguinte conversa por assuntos, agrupando por similaridade {PARTICIPANTS}",
         "novela mexicana": (
-            "Resuma a seguinte conversa, estruturando como um roteiro de novela mexicana (Maria do Bairro)."
-            f" {EXTRA}"
-            " Lembre-se de adicionar muitos dramas, reviravoltas e seja bem exacerbado."
+            "Resuma a seguinte conversa, estruturando como um roteiro de novela mexicana (Maria do Bairro)"
+            f" {PARTICIPANTS} {EXTRA} Lembre-se de adicionar muitos dramas, reviravoltas e seja bem exacerbado."
         ),
         "terror": (
-            f"Resuma a seguinte conversa, estruturando como um roteiro de filme de suspense/terror. {EXTRA}"
-            " Lembre-se de adicionar muito suspense e drama."
+            "Resuma a seguinte conversa, estruturando como um roteiro de filme de suspense/terror"
+            f" {PARTICIPANTS} {EXTRA} Lembre-se de adicionar muito suspense e drama."
         ),
         "faroeste": (
-            f"Resuma a seguinte conversa, estruturando como um roteiro de filme de velho oeste americano. {EXTRA}"
-            " Lembre-se de adicionar muitos tiroteios e duelos."
+            "Resuma a seguinte conversa, estruturando como um roteiro de filme de velho oeste americano"
+            f" {PARTICIPANTS}. {EXTRA} Lembre-se de adicionar muitos tiroteios e duelos."
+        ),
+        "star wars": (
+            "Resuma a seguinte conversa, estruturando como um roteiro de um episódio / filme de Star Wars"
+            f" {PARTICIPANTS}. {EXTRA} Lembre-se de adicionar muitas batalhas espaciais e duelos."
+        ),
+        "senhor dos anéis": (
+            "Resuma a seguinte conversa, estruturando como um roteiro de filme ou livro do Senhor dos Anéis"
+            f" {PARTICIPANTS}. {EXTRA} Lembre-se de adicionar muitas batalhas épicas e aventuras."
+        ),
+        "cyberpunk": (
+            "Resuma a seguinte conversa, estruturando como um roteiro de filme de cyberpunk / sci-fi / futurista"
+            f" {PARTICIPANTS}. {EXTRA} Lembre-se de adicionar muita tecnologia, neon e cenas de ação."
+        ),
+        "ursinho carinhoso": (
+            "Resuma a seguinte conversa, estruturando como um roteiro de desenho animado dos Ursinhos Carinhosos"
+            f" {PARTICIPANTS}. {EXTRA} Lembre-se de adicionar muita fofura e amor (o amor venceu!)."
         ),
     }
 )
