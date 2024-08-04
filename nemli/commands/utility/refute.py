67ebf7fe0356510fd9c6bf310fbd1f54cc72ea97
Mateus Reis <mateuspsvreis@gmail.com>
2024-08-04 18:36:44 -0300

    refactor: refute command is now slash, remove black from pre commit hook

--- nemli/commands/utility/refute.py
+++ nemli/commands/utility/refute.py
@@ -1,4 +1,6 @@
+from discord import CategoryChannel, ForumChannel, Message
 import nextcord
+from nextcord.ext import commands
 from loguru import logger
 
 from nemli import bot
@@ -10,32 +12,64 @@ from nemli.services.messages import parse_discord_messages
 openai_client = OpenAIBot()
 
 
-@bot.message_command(name="refute")
-async def refute_command(interaction: nextcord.Interaction, reference_message: nextcord.Message):
-    logger.debug(f"INFO: The summarize command has been called by: @{interaction.user}")
-    logger.debug(f"Refute message: {reference_message.content}")
+@bot.slash_command(name="refute")
+@commands.cooldown(1, 10, commands.BucketType.channel)
+async def refute_command(interaction: nextcord.Interaction):
+    logger.debug(f"INFO: Refute command called by: @{interaction.user}")
 
-    # We defer the response so we avoid timeout errors due to AI processing which could take longer than three seconds
+    # This is a check to make sure that we are only using this function if the user called it correctly,
+    # which is to refute a given referenced message
+    if not interaction.message or not interaction.message.reference:
+        await interaction.response.send_message(
+            "Você deve usar este comando em resposta a uma mensagem.", ephemeral=True
+        )
+        return
+
+    # This is where the magic happens, we retrieve the 'replied to' message here
+    message_reference_resolved = interaction.message.reference.resolved
+    if message_reference_resolved != Message:
+        await interaction.response.send_message("Não foi possível recuperar a mensagem original.", ephemeral=True)
+        return
+
+    message_reference: Message = message_reference_resolved  # type: ignore
+
+    logger.debug(f"INFO: Message to be refuted: {message_reference.content}")
+
+    # As always since we're dealing with a complex operation (LLM usage) we need to defer the reply
+    # so Discord doesn't shut us off after 3 seconds
     await interaction.response.defer(with_message=True)
 
     try:
-        # Here we are getting the channel where the command was called
         channel = interaction.channel
 
-        # Fetch messages from the channel, after the reference message
-        messages = await channel.history(before=reference_message.created_at, limit=100).flatten()  # type: ignore
-        # Filter messages to only include those from the specified user
-        messages = [msg for msg in messages if msg.author == reference_message.author]
-        # Get the last 10 messages from the user
-        messages = messages[: settings.discord.max_messages_to_refute]
-
-        # Here we are creating a string with the content of the last {message_count} messages
-        parsed_discord_messages: ParserDiscordMessages = parse_discord_messages(bot.user, list(reversed(messages)))
-        if not (parsed_discord_messages.messages):
-            await interaction.followup.send("Não há mensagens suficientes para refutar.")
+        # If somehow the user manages to send a message in a FORUM list of channels
+        # or a CATEGORY list of channels (not actual text channels),
+        # congratulations.. But Discord still tells us it is possible so :shrug:
+        if channel is None or isinstance(channel, (CategoryChannel, ForumChannel)):
+            await interaction.followup.send("Este comando não pode ser usado em uma categoria ou fórum.",
+                                            ephemeral=True)
             return
 
+        # Here we are creating a string with the content of the last {message_count} messages,
+        # this is so the AI has a coherent context to work with,
+        # (mostly due to Dark being a chronically online individual who types phrases in four to five messages)
+        messages = await channel.history(before=message_reference.created_at, limit=100).flatten()
+        messages = [msg for msg in messages if msg.author == message_reference.author]
+
+        # By default we get the last 10 messages starting from the referenced message,
+        # but this is configurable through an env var
+        messages = messages[: settings.discord.max_messages_to_refute]
+
+        # If by any reason we can't recover a coherent context,
+        # we'll not do the function to avoid waste of money and time on the AI
+        parsed_discord_messages: ParserDiscordMessages = parse_discord_messages(bot.user, list(reversed(messages)))
+        if not parsed_discord_messages.messages:
+            await interaction.followup.send("Não há mensagens suficientes para refutar.", ephemeral=True)
+            return
+
+        # Magical string manipulation so the AI can be a little less dumb when figuring out the context from the prompt
         messages_content = " ".join([f"{msg.author}: {msg.content}" for msg in parsed_discord_messages.messages or []])
+
         refuted = openai_client.refute(content=messages_content)
 
         refuted = (
@@ -43,9 +77,10 @@ async def refute_command(interaction: nextcord.Interaction, reference_message: n
             "Seu argumento é inválido, pois há evidências que refutam sua afirmação.\n\n"
             f"{refuted}"
         )
-        await reference_message.reply(refuted)
+
+        await message_reference.reply(refuted)
         await interaction.followup.send(
-            f"@{interaction.user} soliticou que uma mensagem seja refutada!", ephemeral=True
+            f"@{interaction.user} solicitou que uma mensagem seja refutada!", ephemeral=True
         )
     except TimeoutError:
         await interaction.followup.send("Tempo para resposta esgotado.", ephemeral=True)
