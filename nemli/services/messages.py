92d9336ab0ba0bdf2e3d715c6da79a2d1afcb328
Rodolfo De Nadai <rdenadai@gmail.com>
2024-08-05 23:48:37 -0300

    fix: Correct grab the SERVER PROFILE nickname for the user

--- nemli/services/messages.py
+++ nemli/services/messages.py
@@ -14,6 +14,13 @@ def is_summary_link_message(message_content: str) -> bool:
     return message_content.lower().startswith("nemli:")
 
 
+def get_member_name(msg: DiscordMessage) -> str:
+    author_name = msg.author.global_name or msg.author.name
+    if (guild := msg.guild) and (member := guild.get_member(msg.author.id)):
+        return member.nick or author_name or member.display_name
+    return author_name
+
+
 def parse_discord_messages(
     user_bot: Optional[ClientUser] = None,
     messages: Optional[list[DiscordMessage]] = None,
@@ -27,11 +34,12 @@ def parse_discord_messages(
 
         message = None
         if message_content := msg.content:
+            author = get_member_name(msg)
             content = clean_up_stopwords(message_content) if settings.remove_stopwords else message_content
             message = Message(
                 id=msg.id,
                 position=i + 1,
-                author=f"**{msg.author.global_name or msg.author.name}**",
+                author=f"**{author}**",
                 content=content,
                 jump_url=msg.jump_url,
                 created_at=msg.created_at,
